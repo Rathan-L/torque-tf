c414ac3d018dbeb7f281feb930e30a7c0f47c0f8
Rathan-L <111926213+Rathan-L@users.noreply.github.com>
2022-09-26 15:30:18 -0500

    Update outputs.tf

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,8 +1,7 @@
 
-output "public_dns" {
-#  value = aws_instance.myapp.public #something not sure??
-  value = try(aws_instance.this[0].public_dns, "")
-
+output "public_ip" {
+  value = aws_instance.myapp.public_ip #something not sure??
+  
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
 
