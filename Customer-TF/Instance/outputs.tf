17e646642cb5604e16ef315678407414d593ce2b
Rathan-L <111926213+Rathan-L@users.noreply.github.com>
2022-09-26 15:07:02 -0500

    try some fancy try clauses

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,6 +1,7 @@
 
 output "public_dns" {
-  value = aws_instance.myapp.public #something not sure??
+#  value = aws_instance.myapp.public #something not sure??
+  value = try(aws_instance.this[0].public_dns, aws_spot_instance_request.this[0].public_dns, "")
 
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
